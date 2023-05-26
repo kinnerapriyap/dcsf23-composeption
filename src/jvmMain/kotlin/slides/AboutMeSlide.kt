59d35e4155a1904c1dcd42b46869fbf20572b64d
Kinnera Putti <kinnera.putti@ioki.com>
2023-05-26 21:06:40 +0200

    Fix AboutMeSlide

--- src/jvmMain/kotlin/slides/AboutMeSlide.kt
+++ src/jvmMain/kotlin/slides/AboutMeSlide.kt
@@ -1,13 +1,16 @@
 package slides
 
+import aboutMeInfo
 import aboutMeScreenTitle
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
 import common.Heading
 import common.Slide
 
@@ -17,11 +20,18 @@ fun AboutMeSlide(
     name: String,
     modifier: Modifier = Modifier
 ) {
-    Slide {
-        Column(
-            modifier = Modifier.fillMaxSize().padding(40.dp),
-        ) {
-            Heading(pageTitle = aboutMeScreenTitle)
-        }
+    Slide(
+        modifier = modifier,
+        background = MaterialTheme.colors.secondary,
+        contentColor = MaterialTheme.colors.onSecondary,
+    ) {
+        Heading(pageTitle = aboutMeScreenTitle)
+        Text(
+            modifier = Modifier.align(Alignment.Center),
+            text = aboutMeInfo,
+            lineHeight = TextUnit(4f, TextUnitType.Em),
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.h5
+        )
     }
 }
