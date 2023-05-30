02296d22a97f3550051866af7fce6d9351490188
Kinnera Putti <kinnera.putti@ioki.com>
2023-05-30 08:45:13 +0200

    More common

---
+++ src/jvmMain/kotlin/common/Plus.kt
@@ -0,0 +1,24 @@
+package common
+
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+
+@Composable
+@Preview
+fun Plus() {
+    Box(modifier = Modifier.fillMaxHeight()) {
+        Image(
+            modifier = Modifier.size(48.dp).align(Alignment.Center),
+            painter = painterResource("ic_plus.png"),
+            contentDescription = null
+        )
+    }
+}
